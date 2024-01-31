74f2af921990786ad91f28465f77c03a4020110f
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-02-01 02:29:42 +0300

    Add files via upload

---
+++ desktopfile.sh
@@ -0,0 +1,17 @@
+# Prompt the user for their username
+read -p "Enter your username: " username
+
+echo Creating .desktop file for Goverlay
+# Create a .desktop file for Goverlay
+goverlay_desktop_file="/usr/share/applications/goverlay.desktop"
+sudo echo "[Desktop Entry]" > "$goverlay_desktop_file"
+sudo echo "Name=Goverlay" >> "$goverlay_desktop_file"
+sudo echo "Exec=/home/USER_PLACEHOLDER/MangoHudandGoverlay/$extracted_dir/goverlay" >> "$goverlay_desktop_file"
+sudo echo "Icon=/home/USER_PLACEHOLDER/MangoHudandGoverlay/$extracted_dir/goverlay.png" >> "$goverlay_desktop_file"
+sudo echo "Type=Application" >> "$goverlay_desktop_file"
+sudo echo "Categories=Utility;" >> "$goverlay_desktop_file"
+
+# Replace USER_PLACEHOLDER with the actual username
+sed -i "s|USER_PLACEHOLDER|$username|g" "$goverlay_desktop_file"
+echo "\e[0;31mRun the uninstall.sh script if you'd like to uninstall Goverlay & Mangohud. This window will be closed within 5 seconds.\e"
+sleep 5
