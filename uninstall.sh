9ce61c658c3659030a5e24c3348de897436f7039
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-02-01 03:39:22 +0300

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,6 @@
+echo RUN AS NON-ROOT. THE SCRIPT WILL BEGIN IN 5 SECONDS.
+sleep 3
+/home/$USER/MangoHudandGoverlay/MangoHud/mangohud-setup.sh uninstall
+rm -r /home/$USER/MangoHudandGoverlay/
+chmod +x /home/$USER/Downloads/uninstalldesktopfile.sh
+sudo /home/$USER/Downloads/uninstalldesktopfile.sh
